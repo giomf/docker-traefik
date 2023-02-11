91b170b4d76dc74bc5e0a2423b1d6d16813bfd83
Guillaume Fournier <>
2023-02-11 15:43:26 +0100

    remove -- from commands

--- README.md
+++ README.md
@@ -5,18 +5,18 @@
 The ctl.sh script provides commandos that are useful to control the services
 ### Usage
 ```
-$ ./ctl.sh 
+$ ./ctl.sh help
 Control script for services
 Usage: ctl.sh
-        --start         Starts all services
-        --stop          Stops all services
-        --soft-restart  Restart all services
-        --hard-restart  Stops and Starts all services
-        --logs          Shows the docker logs
-        --status        Shows the service status
-        --init          Initializes the volumes and copies configuration files to the volumes.
+        start           Starts all services
+        stop            Stops all services
+        restart         Restart all services
+        reset           Stops and Starts all services
+        logs            Shows the docker logs
+        status          Shows the services status
+        init            Initializes the volumes and copies configuration files to the volumes.
                         This is only needed the first time!
-        --help          Shows this help page
+        help            Shows this help page
 ```
 ### Init behavior
 
--- ctl.sh
+++ ctl.sh
@@ -5,32 +5,32 @@ function init (){
     exit 1
 }
 
-if [[ -z "$1" || "$1" == "--help" ]]; then
+if [[ -z "$1" || "$1" == "help" ]]; then
     script_name="$(basename "$0")"
     echo "Control script for services"
     echo "Usage: ${script_name}"
-    echo -e "\t--start\t\tStarts all services"
-    echo -e "\t--stop\t\tStops all services"
-    echo -e "\t--soft-restart\tRestart all services"
-    echo -e "\t--hard-restart\tStops and Starts all services"
-    echo -e "\t--logs\t\tShows the docker logs"
-    echo -e "\t--status\tShows the service status"
-    echo -e "\t--init\t\tInitializes the volumes and copies configuration files to the volumes.\n\t\t\tThis is only needed the first time!"
-    echo -e "\t--help\t\tShows this help page"
+    echo -e "\tstart\t\tStarts all services"
+    echo -e "\tstop\t\tStops all services"
+    echo -e "\trestart\t\tRestart all services"
+    echo -e "\treset\t\tStops and Starts all services"
+    echo -e "\tlogs\t\tShows the docker logs"
+    echo -e "\tstatus\t\tShows the services status"
+    echo -e "\tinit\t\tInitializes the volumes and copies configuration files to the volumes.\n\t\t\tThis is only needed the first time!"
+    echo -e "\thelp\t\tShows this help page"
     
-elif [[ "$1" == "--start" ]]; then
+elif [[ "$1" == "start" ]]; then
     docker-compose up -d
-elif [[ "$1" == "--stop" ]]; then
+elif [[ "$1" == "stop" ]]; then
     docker-compose down
-elif [[ "$1" == "--soft-restart" ]]; then
+elif [[ "$1" == "restart" ]]; then
     docker-compose restart
-elif [[ "$1" == "--hard-restart" ]]; then
+elif [[ "$1" == "reset" ]]; then
     docker-compose down && docker-compose up -d
-elif [[ "$1" == "--logs" ]]; then
+elif [[ "$1" == "logs" ]]; then
     docker-compose logs -f
-elif [[ "$1" == "--status" ]]; then
+elif [[ "$1" == "status" ]]; then
     docker-compose ps
-elif [[ "$1" == "--init" ]]; then
+elif [[ "$1" == "init" ]]; then
 
     if [[ -d volumes/ ]]; then
         echo "ERROR: volumes already exists. Aborting!"
@@ -40,7 +40,7 @@ elif [[ "$1" == "--init" ]]; then
     fi
 else 
     echo "ERROR: Command $1 not found. Aborting!"
-    echo "Use --help for more informations"
+    echo "Use help for more informations"
     exit 1
 fi
 
